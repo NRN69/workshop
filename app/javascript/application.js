db4a96476eaf644f03e319cd34d2205321fa15f5
NRN69 <HARRY-B@MAIL.RU>
2023-01-29 00:26:52 +0300

    Add modal window 'Sign in'

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,6 +4,25 @@ import "./controllers"
 import * as ActiveStorage from "@rails/activestorage"
 import * as bootstrap from "bootstrap"
 
+
+export default class extends Controller {
+    connect() {
+        this.modal = new bootstrap.Modal(this.element)
+    }
+
+    open() {
+        if (!this.modal.isOpened) {
+            this.modal.show()
+        }
+    }
+
+    close(event) {
+        if (event.detail.success) {
+            this.modal.hide()
+        }
+    }
+}
+
 import Button from 'bootstrap/js/dist/button';
 import Collapse from 'bootstrap/js/dist/collapse';
 import Alert from 'bootstrap/js/dist/alert';
