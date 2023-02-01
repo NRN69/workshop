ad0d17b91044a9f3d2fe87f42f017eae316282c5
NRN69 <HARRY-B@MAIL.RU>
2023-02-01 17:56:28 +0300

    Fixed bug loading custom.css Add change I18n locale

---
+++ app/controllers/concerns/Authorization.rb
@@ -0,0 +1,21 @@
+module Authorization
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :store_user_location!, if: :storable_location?
+
+    private
+
+    def storable_location?
+      request.get? &&
+        is_navigational_format? &&
+        !devise_controller? &&
+        !request.xhr? &&
+        !turbo_frame_request?
+    end
+
+    def store_user_location!
+      store_location_for(:user, request.fullpath)
+    end
+  end
+end
