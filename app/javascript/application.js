94fb3634a9e31e8142dcf3f0feda382d01a07ae6
NRN69 <HARRY-B@MAIL.RU>
2023-02-18 02:59:45 +0300

    Fixed load esbuild Add Footer Add Contacts

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,28 +1,5 @@
-// Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
-import "./controllers"
 import * as ActiveStorage from "@rails/activestorage"
-import * as bootstrap from "bootstrap"
-
-
-export default class extends Controller {
-    connect() {
-        this.modal = new bootstrap.Modal(this.element)
-    }
-
-    open() {
-        if (!this.modal.isOpened) {
-            this.modal.show()
-        }
-    }
-
-    close(event) {
-        if (event.detail.success) {
-            this.modal.hide()
-        }
-    }
-}
-
+import "./ScrollTopButton"
 
 ActiveStorage.start();
-bootstrap.start();
